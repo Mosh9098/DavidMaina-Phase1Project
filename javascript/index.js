df4831598202542f1ecb9d85147e12752cb98be2
mosh9098 <mosh9098@gmail.com>
2024-04-09 15:57:11 +0300

    complete

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+const pokemonNameInput = document.getElementById('pokemon-name');
+const generateCardButton = document.getElementById('generate-card');
+const pokemonCard = document.getElementById('pokemon-card');
+
+generateCardButton.addEventListener('click', () => {
+  const pokemonName = pokemonNameInput.value.toLowerCase();
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.id) {
+        generateCard(data);
+      } else {
+        pokemonCard.innerHTML = 'Pokemon not found!';
+      }
+      console.log(data);
+    })
+    .catch(error => {
+      pokemonCard.innerHTML = 'Error fetching Pokemon data!';
+    });
+});
+function generateCard(pokemonData) {
+  fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`)
+    .then(response => response.url)
+    .then(spriteUrl => {
+      const cardHTML = `
+        <img src="${spriteUrl}" alt="${pokemonData.name}">
+        <h2>${pokemonData.name}</h2>
+        <p>Type: ${pokemonData.types[0].type.name}</p>
+      `;
+      pokemonCard.innerHTML = cardHTML;
+    });}
+});

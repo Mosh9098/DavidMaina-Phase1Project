8fad0fa46298c8dd88b7b82e5a279f746a65d05d
mosh9098 <mosh9098@gmail.com>
2024-04-11 15:16:31 +0300

    complete

--- javascript/index.js
+++ javascript/index.js
@@ -1,33 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-const pokemonNameInput = document.getElementById('pokemon-name');
-const generateCardButton = document.getElementById('generate-card');
-const pokemonCard = document.getElementById('pokemon-card');
+  const pokemonNameInput = document.getElementById('pokemon-name');
+  const generateCardButton = document.getElementById('generate-card');
+  const pokemonCard = document.getElementById('pokemon-card');
 
-generateCardButton.addEventListener('click', () => {
-  const pokemonName = pokemonNameInput.value.toLowerCase();
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    .then(response => response.json())
-    .then(data => {
-      if (data.id) {
-        generateCard(data);
-      } else {
-        pokemonCard.innerHTML = 'Enter Pokemon Name!';
-      }
-      console.log(data);
-    })
-    .catch(error => {
-      pokemonCard.innerHTML = 'Pokemon not found!';
-    });
-});
-function generateCard(pokemonData) {
-  fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`)
-    .then(response => response.url)
-    .then(spriteUrl => {
-      const cardHTML = `
-        <img src="${spriteUrl}" alt="${pokemonData.name}">
-        <h2>${pokemonData.name}</h2>
-        <p>Type: ${pokemonData.types[0].type.name}</p>
-      `;
-      pokemonCard.innerHTML = cardHTML;
-    });}
+  generateCardButton.addEventListener('click', () => {
+    const pokemonName = pokemonNameInput.value.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+     .then(response => response.json())
+     .then(data => {
+        if (data.id) {
+          generateCard(data);
+        } else {
+          pokemonCard.innerHTML = 'Enter Pokemon Name!';
+        }
+      })
+     .catch(error => {
+        pokemonCard.innerHTML = 'Pokemon not found!';
+      });
+  });
+
+  function generateCard(pokemonData) {
+    fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`)
+     .then(response => response.url)
+     .then(spriteUrl => {
+        const cardHTML = `
+          <img src="${spriteUrl}" alt="${pokemonData.name}">
+          <h2>${pokemonData.name}</h2>
+          <p>Type: ${pokemonData.types[0].type.name}</p>
+          <div id="like"> 
+          <button id="like-button">👍</button>
+          <button id="dislike-button">👎</button>
+          <div>
+        `;
+        pokemonCard.innerHTML = cardHTML;
+
+        const likeButton = document.getElementById('like-button');
+        const dislikeButton = document.getElementById('dislike-button');
+
+        likeButton.addEventListener('click', () => {
+          alert(`You liked ${pokemonData.name}!`);
+        });
+
+        dislikeButton.addEventListener('click', () => {
+          alert(`You disliked ${pokemonData.name}!`);
+        });
+      });
+  }
 });

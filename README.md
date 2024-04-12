ea1226cd0e615f3c2ac660601e1ffc066aa501b7
mosh9098 <mosh9098@gmail.com>
2024-04-12 13:31:05 +0300

    complete

---
+++ README.md
@@ -0,0 +1,45 @@
+# DAVID MAINA-Phase 1 Independent Project
+# (pokemon Card Generator)
+
+#### Date, 2024/04/11
+
+#### By *DAVID MAINA*
+
+## Description
+This is a simple web application that generates Pokemon cards based on user input. Users 
+can enter the name of a Pokemon, and the application will fetch data from the PokeAPI to 
+generate a card displaying the Pokemon's image, name, and type. The card also includes 
+like and dislike buttons for users to express their preferences, as well as a comment 
+section where users can leave comments about the displayed Pokemon.
+
+## Installation
+You use git clone to be able to download the documents in the GitHub on the terminal : 
+git clone https://github.com/Mosh9098/DavidMaina-Phase1Project.git
+
+## Installation Requirements
+To run this project, you need to have the following software installed:
+
+- Node.js 
+- npm 
+- Git
+
+### Installation instruction
+```
+
+Git clone https://github.com/Mosh9098/DavidMaina-Phase1Project.git
+```
+
+# Live Link
+[Git](https://mosh9098.github.io/DavidMaina-Phase1Project/)
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- PokeAPI
+
+
+### License
+This project is licensed under the MIT License. Copyright (c) 2024.
+Copyright (c) David Maina 2024.
--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,35 @@
+// This event listener ensures that the JavaScript code executes
 document.addEventListener('DOMContentLoaded', () => {
-  const pokemonNameInput = document.getElementById('pokemon-name');
+// Get references to the necessary HTML elements  const pokemonNameInput = document.getElementById('pokemon-name');
   const generateCardButton = document.getElementById('generate-card');
   const pokemonCard = document.getElementById('pokemon-card');
-
+// Event listener for the "Generate Card" button
   generateCardButton.addEventListener('click', () => {
     const pokemonName = pokemonNameInput.value.toLowerCase();
+// Fetch data from the PokeAPI using the provided Pokemon name
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
      .then(response => response.json())
      .then(data => {
+// If the response contains the ID of the Pokemon, generate the card      
         if (data.id) {
           generateCard(data);
+// If no ID is found, display an error message          
         } else {
           pokemonCard.innerHTML = 'Enter Pokemon Name!';
         }
       })
+// If an error occurs during the fetch request, display an error message
      .catch(error => {
         pokemonCard.innerHTML = 'Pokemon not found!';
       });
   });
-
+// Function to generate the Pokemon card
   function generateCard(pokemonData) {
+// Fetch the URL for the Pokemon sprite image
     fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`)
      .then(response => response.url)
      .then(spriteUrl => {
+// Creating the HTML for the Pokemon card using the fetched data
         const cardHTML = `
           <img src="${spriteUrl}" alt="${pokemonData.name}">
           <h2>${pokemonData.name}</h2>
@@ -38,13 +45,15 @@ document.addEventListener('DOMContentLoaded', () => {
           </div>
           </div>
         `;
+// Insert the HTML into the Pokemon card element
         pokemonCard.innerHTML = cardHTML;
-
+// Get references to the like and dislike buttons, comment box, and submit button
         const likeButton = document.getElementById('like-button');
         const dislikeButton = document.getElementById('dislike-button');
         const commentBox = document.getElementById('comment-box');
         const submitButton = document.getElementById('submit-comment');
 
+// Event listener for the like and dislike buttons and alert for either
         likeButton.addEventListener('click', () => {
           alert(`You liked ${pokemonData.name}!`);
         });
@@ -52,10 +61,13 @@ document.addEventListener('DOMContentLoaded', () => {
         dislikeButton.addEventListener('click', () => {
           alert(`You disliked ${pokemonData.name}!`);
         });
+// Event listener for the submit button
         submitButton.addEventListener('click', () => {
-          const comment = commentBox.value.trim(); // Get the comment text and remove leading/trailing spaces
+          const comment = commentBox.value.trim(); 
+// If a comment was entered, alert the user with the comment text
           if (comment) {
             alert(`Comment received: ${comment}`);
+// If no comment was entered, alert the user to enter a comment
           } else {
             alert("Please enter a comment.");
           }
